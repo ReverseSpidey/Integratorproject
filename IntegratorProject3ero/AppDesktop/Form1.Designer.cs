93f22ea03c112159fcd160f67763cc1b1f5b7f77
ReverseSpidey <ReverseSpidey@users.noreply.github.com>
2018-05-14 20:01:53 -0500

    Empieza

---
+++ IntegratorProject3ero/AppDesktop/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace AppDesktop
+{
+    partial class frmApp
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ppnlHeader = new System.Windows.Forms.Panel();
+            this.pnlBody = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // ppnlHeader
+            // 
+            this.ppnlHeader.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(176)))), ((int)(((byte)(0)))), ((int)(((byte)(32)))));
+            this.ppnlHeader.Dock = System.Windows.Forms.DockStyle.Top;
+            this.ppnlHeader.Location = new System.Drawing.Point(0, 0);
+            this.ppnlHeader.Name = "ppnlHeader";
+            this.ppnlHeader.Size = new System.Drawing.Size(875, 104);
+            this.ppnlHeader.TabIndex = 0;
+            // 
+            // pnlBody
+            // 
+            this.pnlBody.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(176)))), ((int)(((byte)(0)))), ((int)(((byte)(32)))));
+            this.pnlBody.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pnlBody.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.pnlBody.Location = new System.Drawing.Point(0, 104);
+            this.pnlBody.Name = "pnlBody";
+            this.pnlBody.Size = new System.Drawing.Size(875, 383);
+            this.pnlBody.TabIndex = 1;
+            // 
+            // frmApp
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(875, 487);
+            this.Controls.Add(this.pnlBody);
+            this.Controls.Add(this.ppnlHeader);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "frmApp";
+            this.Text = "App";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel ppnlHeader;
+        private System.Windows.Forms.Panel pnlBody;
+    }
+}
+
